40634295b30c52cc05361cdcd93f069629eec0a0
Mohamed Osama <mossama370@gmail.com>
2024-04-22 19:18:33 +0200

    java

--- 0x00-ES6_basic/9-getFullBudget.js
+++ 0x00-ES6_basic/9-getFullBudget.js
@@ -1,13 +1,13 @@
-import getBudgetObject from './7-getBudgetObject.js';
+import getBudgetObject from './7-getBudgetObject';
 
 export default function getFullBudgetObject(income, gdp, capita) {
   const budget = getBudgetObject(income, gdp, capita);
   const fullBudget = {
     ...budget,
-    getIncomeInDollars() {
+    getIncomeInDollars(income) {
       return `$${income}`;
     },
-    getIncomeInEuros() {
+    getIncomeInEuros(income) {
       return `${income} euros`;
     },
   };
