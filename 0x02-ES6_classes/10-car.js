53b43b66f5fb396020d1cea9889fc2ff872d8a90
Mohamed Osama <mossama370@gmail.com>
2024-04-25 18:06:11 +0200

    java

--- 0x02-ES6_classes/10-car.js
+++ 0x02-ES6_classes/10-car.js
@@ -1,7 +1,3 @@
-const _brand = Symbol('brand');
-const _motor = Symbol('motor');
-const _color = Symbol('color');
-
 export default class Car {
   constructor(brand, motor, color) {
     this._brand = brand;
@@ -9,11 +5,7 @@ export default class Car {
     this._color = color;
   }
 
-  static get [Symbol.species]() {
-    return this;
-  }
-
   cloneCar() {
-    return new Car(this[_brand], this[_motor], this[_color]);
+    return new this.constructor();
   }
 }
